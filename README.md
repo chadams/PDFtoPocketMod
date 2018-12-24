fae84195f8ffdff5902ef7b9087f9082fa622b88
Chad Adams <chad@variantllc.com>
2018-12-24 15:38:30 -0500

    testing letter

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PDFtoPocketMod
+
 PDF to PocketMod Converter
+
+nvm use 8.11.3
+npm install
--- test.js
+++ test.js
@@ -3,5 +3,5 @@ const PocketModConverter = require("./src/pm");
 const outputFile = __dirname + "/tests/output/test.pdf";
 const srcFile = __dirname + "/tests/TestMaterials/BasicTIFFImagesTest.pdf";
 
-const pocketmod = new PocketModConverter();
+const pocketmod = new PocketModConverter({ size: "letter" });
 pocketmod.convert(srcFile, outputFile);
